ecc091026527620686fb1b69664d27220c842a1c
Armir <mirarmirkhan@gmail.com>
2023-03-21 14:02:41 +0500

    add js code

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
+"use strict";
 const audio = new Audio("ES_Button Switch Click 1 - SFX Producer.mp3");
 const buttons = document.querySelectorAll("button");
-"use strict";
+const pomodoro = document.querySelector(".pomodoro");
+const short = document.querySelector(".short-break");
+const long = document.querySelector(".long-break");
+const timer = document.querySelector(".timer");
+
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
       audio.play();
     });
-  });
+});
+
 var cursor = document.querySelector(".cursor");
 var cursor2 = document.querySelector(".cursor2");
 document.addEventListener("mousemove", function (e) {
   cursor.style.cssText = cursor2.style.cssText =
     "left: " + e.clientX + "px; top:" + e.clientY + "px;";
 });
+
