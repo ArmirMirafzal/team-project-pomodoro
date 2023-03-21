190c85dcd120c5b21ada947ae2205a0210d71bc2
gang_dramma <your_email@kottaholakottahola@gmail.com>
2023-03-21 14:14:24 +0500

    change mirafzal stupid codes

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const pomodoro = document.querySelector(".pomodoro");
 const short = document.querySelector(".short-break");
 const long = document.querySelector(".long-break");
 const timer = document.querySelector(".timer");
+const tbox = document.querySelector(".timer-box");
+const start = document.querySelector(".start");
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
@@ -19,3 +21,21 @@ document.addEventListener("mousemove", function (e) {
     "left: " + e.clientX + "px; top:" + e.clientY + "px;";
 });
 
+short.addEventListener("click", function (e) {
+  timer.textContent = "5:00";
+  tbox.style.background = "#4C9196";
+  start.style.color = "#4C9196";
+  document.body.style.background = "#38858A";
+});
+pomodoro.addEventListener("click", function (e) {
+  timer.textContent = "25:00";
+  tbox.style.background = "#C15C5C";
+  start.style.color = "#C15C5C";
+  document.body.style.background = "#BA4949";
+});
+long.addEventListener("click", function (e) {
+  timer.textContent = "15:00";
+  tbox.style.background = "#4D7FA2";
+  start.style.color = "#4D7FA2";
+  document.body.style.background = "#397097";
+});
